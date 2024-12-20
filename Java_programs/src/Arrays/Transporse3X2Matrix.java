f5e7a4b5f45450ce8f8a0b0004a886b80831d0a0
RevuBhargav' <bhargavrevu58@gmail.com>
2024-12-20 16:29:19 +0530

    n

--- Java_programs/src/Arrays/Transporse3X2Matrix.java
+++ Java_programs/src/Arrays/Transporse3X2Matrix.java
@@ -1,7 +1,10 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Transporse3X2Matrix {
 public static void main(String[] args) {
+	
 	int arr[][] = {{1,2},{3,4},{5,6}};
 	for(int i=0; i< arr.length;i++) {
 		for(int j=0;j<arr[i].length;j++) {
